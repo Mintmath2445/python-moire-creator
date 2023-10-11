70b6cea139b67572788a68c00b69b622f2e81398
Mintmath2445 <mlugoocampo@gmail.com>
2023-10-11 02:12:29 -0400

    first commit

---
+++ Main.py
@@ -0,0 +1,101 @@
+#imports
+import math
+import numpy as np
+from PIL import Image as im, ImageDraw as imdraw
+
+# create array filled with white pixels 500x500, with the variables
+# x is horizontal limit, y is vertical limit and d is the step between each line, in pixels
+# w is the width of each line
+print("input dimensions of image")
+while True :
+    x = int(input())
+    if isinstance(x, int) == False :
+        print("input integer")
+    else:
+        break
+
+while True :
+    y = int(input())
+    if isinstance(y, int) == False :
+        print("input integer")
+    else:
+        break
+    
+print("input step of first set of lines")
+while True :
+    d = int(input())
+    if isinstance(d, int) == False :
+        print("input integer")
+    else:
+        break
+    
+print("input step of second set of lines")
+while True :
+    delta = int(input())
+    if isinstance(delta, int) == False :
+        print("input integer")
+    else:
+        break
+        
+#for testing
+print("x= "+str(x))
+print("y= "+str(y))
+print("d= "+str(d))
+print("delta= "+str(delta))
+w = 5
+array = np.full((y, x, 3), 
+                        255, dtype = np.uint8) 
+
+#prints the array
+#print(array)
+
+#saves the array into im format to be transformed into png
+#draw will draw over original image, avec un reseau de lignes 
+#NOTE the order is (x,y) so 0,250, 500,250 will draw a horizontal line
+
+data = im.fromarray (array)
+draw = imdraw.Draw(data) 
+#reseau de lignes #1
+i = 0
+while i<=x//d :
+    draw.line((0+i*d,0 , 0+i*d,y), fill=0, width=w)
+    i=i+1
+    #if i==50:
+        #break
+#print ("End of loop #1")
+
+data.save("reseau1.png")
+
+#reseau de lignes #2
+draw2 = imdraw.Draw(data)
+n = 0
+while n<=x//delta :
+    draw2.line((0+n*delta,0 , 0+n*delta,y), fill=0, width=w) 
+    n=n+1
+   # if n==100:
+        #break
+#print ("End of loop #2")
+
+data.save("moire.png")
+
+#moire avec quelques zones d'interet qui ont ete surlignees
+#trouver le ppcm
+ppcm = math.lcm(d, delta)
+
+draw3 = imdraw.Draw(data)
+draw4 = imdraw.Draw(data)
+#rouge = zone clair, vert = zone sombre
+m=0
+while m<=x//ppcm :
+    draw3.line((m*ppcm,0 , m*ppcm,y), fill= (230, 0, 0), width=w)
+    draw4.line((ppcm//2 + m*ppcm,0 , ppcm//2 + m*ppcm,y), fill=(0, 230, 0), width=w)
+    m=m+1
+
+data.save("zones.png")
+
+#file_object = open("zoneclaire.png", "r")
+
+#end message
+print ("\nB)\n")
+print ("!!! All done !!!\n")
+print (":]\n")
---
+++ README.md
@@ -0,0 +1,16 @@
+For the moment,
+Use a source-code editor such as vscode
+
+In Main.py, run and open a terminal
+input the required parameters
+
+the dimensions are x, then y in pixels, for an image of x times y resolution
+
+the step is the distance between lines in pixels, one for each set of lines
+
+the output in the terminal will print out the given parameters
+
+It should create 3 pngs, reseau1.png, which is the png of the first set of lines
+moire.png, the moire pattern resulting by the combination with the second set of lines
+zones.png, the moire pattern with the middle of the light areas highlighted as well as the middle
+of the dark areas highlighted
